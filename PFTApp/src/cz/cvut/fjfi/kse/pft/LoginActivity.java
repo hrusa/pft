7abd6d2a18ded1ff7cacfe0162d5328744dd483d
hrusa <Pjetrek@centrum.cz>
2014-04-01 18:40:47 +0200

    Login via Google+ acc with fragments

--- PFTApp/src/cz/cvut/fjfi/kse/pft/LoginActivity.java
+++ PFTApp/src/cz/cvut/fjfi/kse/pft/LoginActivity.java
@@ -1,60 +1,23 @@
 package cz.cvut.fjfi.kse.pft;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.plus.Plus;
-
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Intent;
-import android.content.IntentSender.SendIntentException;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.View.OnClickListener;
-import android.widget.Toast;
 
-public class LoginActivity extends FragmentActivity implements
-ConnectionCallbacks, OnConnectionFailedListener, OnClickListener{
+public class LoginActivity extends FragmentActivity {
 
-	/* Request code used to invoke sign in user interactions. */
-	private static final int RC_SIGN_IN = 0;
-	/* Client used to interact with Google APIs. */
-	private GoogleApiClient mGoogleApiClient;
-	/* A flag indicating that a PendingIntent is in progress and prevents
-	 * us from starting further intents.
-	 */
-	private boolean mIntentInProgress;
-	/* Track whether the sign-in button has been clicked so that we know to resolve
-	 * all issues preventing sign-in without waiting.
-	 */
-	private boolean mSignInClicked;
-	/* Store the connection result from onConnectionFailed callbacks so that we can
-	 * resolve them when the user clicks sign-in.
-	 */
-	private ConnectionResult mConnectionResult;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 
 		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new LoginFragment()).commit();
 		}
-		
-		mGoogleApiClient = new GoogleApiClient.Builder(this)
-        .addConnectionCallbacks(this)
-        .addOnConnectionFailedListener(this)
-        .addApi(Plus.API, null)
-        .addScope(Plus.SCOPE_PLUS_LOGIN)
-        .build();
 	}
 
 	@Override
@@ -76,119 +39,11 @@ ConnectionCallbacks, OnConnectionFailedListener, OnClickListener{
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_login,
-					container, false);
-			return rootView;
-		}
-	}
 	
-	protected void onStart() {
-	    super.onStart();
-	    mGoogleApiClient.connect();
-	}
-
-	protected void onStop() {
-	    super.onStop();
-
-	    if (mGoogleApiClient.isConnected()) {
-	      mGoogleApiClient.disconnect();
-	    }
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener#onConnectionFailed(com.google.android.gms.common.ConnectionResult)
-	 */
 	@Override
-	public void onConnectionFailed(ConnectionResult result) {
-		// TODO Auto-generated method stub
-		if (!mIntentInProgress) {
-		    // Store the ConnectionResult so that we can use it later when the user clicks
-		    // 'sign-in'.
-		    mConnectionResult = result;
-
-		    if (mSignInClicked) {
-		      // The user has already clicked 'sign-in' so we attempt to resolve all
-		      // errors until the user is signed in, or they cancel.
-		      resolveSignInError();
-		    }
-		}
-	}
-
-	/* (non-Javadoc)
-	 * @see com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks#onConnected(android.os.Bundle)
-	 */
-	@Override
-	public void onConnected(Bundle arg0) {
-		// TODO Auto-generated method stub
-		mSignInClicked = false;
-		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
-	}
-
-	/* (non-Javadoc)
-	 * @see com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks#onConnectionSuspended(int)
-	 */
-	@Override
-	public void onConnectionSuspended(int arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 	protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
-		if (requestCode == RC_SIGN_IN) {
-		    if (responseCode != RESULT_OK) {
-		      mSignInClicked = false;
-		    }
-
-		    mIntentInProgress = false;
-
-		    if (!mGoogleApiClient.isConnecting()) {
-		      mGoogleApiClient.connect();
-		    }
-		}
+		Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+		if (fragment instanceof LoginFragment)
+		    ((LoginFragment) fragment).connect(requestCode, responseCode, intent);
 	}
-	
-	/* A helper method to resolve the current ConnectionResult error. */
-	private void resolveSignInError() {
-	  if (mConnectionResult.hasResolution()) {
-	    try {
-	      mIntentInProgress = true;
-	      mConnectionResult.startResolutionForResult(this, // your activity
-                  RC_SIGN_IN);
-	    } catch (SendIntentException e) {
-	      // The intent was canceled before it was sent.  Return to the default
-	      // state and attempt to connect to get an updated ConnectionResult.
-	      mIntentInProgress = false;
-	      mGoogleApiClient.connect();
-	    }
-	  }
-	}
-	
-	public void onClick(View view) {
-		  if (view.getId() == R.id.sign_in_button
-		    && !mGoogleApiClient.isConnecting()) {
-		    mSignInClicked = true;
-		    resolveSignInError();
-		  }
-		  
-		  if (view.getId() == R.id.sign_out_button) {
-			    if (mGoogleApiClient.isConnected()) {
-			      Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
-			      mGoogleApiClient.disconnect();
-			      mGoogleApiClient.connect();
-			    }
-		  }
-	}
-
 }
--- PFTApp/src/cz/cvut/fjfi/kse/pft/LoginFragment.java
+++ PFTApp/src/cz/cvut/fjfi/kse/pft/LoginFragment.java
@@ -1,34 +1,36 @@
+/**
+ * 
+ */
 package cz.cvut.fjfi.kse.pft;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.People;
 import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.People.LoadPeopleResult;
+import com.google.android.gms.plus.model.people.Person;
 
-import cz.cvut.fjfi.kse.pft.R;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
- * An activity representing a single Item detail screen. This activity is only
- * used on handset devices. On tablet-size devices, item details are presented
- * side-by-side with a list of items in a {@link ItemListActivity}.
- * <p>
- * This activity is mostly just a 'shell' activity containing nothing more than
- * a {@link ItemDetailFragment}.
+ * @author Petr Hruška
+ *
  */
-public class ItemDetailActivity extends FragmentActivity implements
-	ConnectionCallbacks, OnConnectionFailedListener, OnClickListener {
-	
+public class LoginFragment extends Fragment implements ConnectionCallbacks, OnConnectionFailedListener, OnClickListener, ResultCallback<People.LoadPeopleResult> {
 	/* Request code used to invoke sign in user interactions. */
 	private static final int RC_SIGN_IN = 0;
 	/* Client used to interact with Google APIs. */
@@ -45,94 +47,85 @@ public class ItemDetailActivity extends FragmentActivity implements
 	 * resolve them when the user clicks sign-in.
 	 */
 	private ConnectionResult mConnectionResult;
+	
+	private TextView mUser;
+	
+	/**
+	 * 
+	 */
+	public LoginFragment() {
+		// TODO Auto-generated constructor stub
+	}
 
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.Fragment#onCreate(android.os.Bundle)
+	 */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_item_detail);
-
-		// Show the Up button in the action bar.
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-
-		// savedInstanceState is non-null when there is fragment state
-		// saved from previous configurations of this activity
-		// (e.g. when rotating the screen from portrait to landscape).
-		// In this case, the fragment will automatically be re-added
-		// to its container so we don't need to manually add it.
-		// For more information, see the Fragments API guide at:
-		//
-		// http://developer.android.com/guide/components/fragments.html
-		//
-//		if (savedInstanceState == null) {
-//			// Create the detail fragment and add it to the activity
-//			// using a fragment transaction.
-//			Bundle arguments = new Bundle();
-//			arguments.putString(ItemDetailFragment.ARG_ITEM_ID, getIntent()
-//					.getStringExtra(ItemDetailFragment.ARG_ITEM_ID));
-//			ItemDetailFragment fragment = new ItemDetailFragment();
-//			fragment.setArguments(arguments);
-//			getSupportFragmentManager().beginTransaction()
-//					.add(R.id.item_detail_container, fragment).commit();
-//		}
+		Log.i("onCreate: ", "started");
 		
-		findViewById(R.id.sign_in_button).setOnClickListener(this);
-		
-		mGoogleApiClient = new GoogleApiClient.Builder(this)
+		mGoogleApiClient = new GoogleApiClient.Builder(getActivity())	//možná změníme parametr na getActivity().getBaseContext()
         .addConnectionCallbacks(this)
         .addOnConnectionFailedListener(this)
         .addApi(Plus.API, null)
         .addScope(Plus.SCOPE_PLUS_LOGIN)
         .build();
+		Log.i("onCreate: ", "init");
 	}
 
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	 */
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpTo(this,
-					new Intent(this, ItemListActivity.class));
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View rootView = inflater.inflate(R.layout.fragment_login,
+				container, false);
+		mUser = (TextView) rootView.findViewById(R.id.sign_in_status);
+		rootView.findViewById(R.id.sign_in_button).setOnClickListener(this);
+		Log.i("onCreateView: ", "layout init");
+		return rootView;
 	}
 	
 	@Override
-	protected void onStart() {
+	public void onStart() {
 	    super.onStart();
 	    mGoogleApiClient.connect();
+	    Log.i("onStart: ", "started");
 	}
 
 	@Override
-	protected void onStop() {
+	public void onStop() {
 	    super.onStop();
 
 	    if (mGoogleApiClient.isConnected()) {
 	      mGoogleApiClient.disconnect();
+	      Log.i("onStop: ", "disconnect");
 	    }
+	    Log.i("onStop: ", "ended");
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener#onConnectionFailed(com.google.android.gms.common.ConnectionResult)
 	 */
 	@Override
 	public void onConnectionFailed(ConnectionResult result) {
 		// TODO Auto-generated method stub
+		Log.i("onCF: ", "started");
 		if (!mIntentInProgress) {
 		    // Store the ConnectionResult so that we can use it later when the user clicks
 		    // 'sign-in'.
+			Log.i("onCF: ", "1 if");
 		    mConnectionResult = result;
 
 		    if (mSignInClicked) {
 		      // The user has already clicked 'sign-in' so we attempt to resolve all
 		      // errors until the user is signed in, or they cancel.
 		      resolveSignInError();
+		      Log.i("onCF: ", "2 if");
 		    }
 		}
 	}
@@ -143,8 +136,17 @@ public class ItemDetailActivity extends FragmentActivity implements
 	@Override
 	public void onConnected(Bundle arg0) {
 		// TODO Auto-generated method stub
+		Log.i("onConnected: ", "started");
 		mSignInClicked = false;
-		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+		Toast.makeText(getActivity(), "User is connected!", Toast.LENGTH_LONG).show();
+		Person currentUser = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+	    
+	    mUser.setText(String.format(
+	        getResources().getString(R.string.status_sign_in),
+	        currentUser.getDisplayName()));	   
+
+	    Plus.PeopleApi.loadVisible(mGoogleApiClient, null)
+	        .setResultCallback(this);
 	}
 
 	/* (non-Javadoc)
@@ -153,46 +155,72 @@ public class ItemDetailActivity extends FragmentActivity implements
 	@Override
 	public void onConnectionSuspended(int arg0) {
 		// TODO Auto-generated method stub
-		
+		Log.i("onConnectionSuspended: ", "started");
 	}
 	
-	@Override
-	protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+	public void connect(int requestCode, int responseCode, Intent intent) {
+		Log.i("connect: ", "started");
 		if (requestCode == RC_SIGN_IN) {
-		    if (responseCode != RESULT_OK) {
+			Log.i("connect: ", "1 if");
+		    if (responseCode != Activity.RESULT_OK) {
 		      mSignInClicked = false;
+		      Log.i("connect: ", "2.1 if");
 		    }
 
 		    mIntentInProgress = false;
 
 		    if (!mGoogleApiClient.isConnecting()) {
 		      mGoogleApiClient.connect();
+		      Log.i("connect: ", "2.2 if");
 		    }
 		}
 	}
 	
 	/* A helper method to resolve the current ConnectionResult error. */
 	private void resolveSignInError() {
+		Log.i("resolveSignInError: ", "started");
 	  if (mConnectionResult.hasResolution()) {
+		  Log.i("resolveSignInError: ", "if");  
 	    try {
 	      mIntentInProgress = true;
-	      mConnectionResult.startResolutionForResult(this, // your activity
+	      mConnectionResult.startResolutionForResult(getActivity(), // your activity
                   RC_SIGN_IN);
+	      Log.i("resolveSignInError: ", "try");
 	    } catch (SendIntentException e) {
 	      // The intent was canceled before it was sent.  Return to the default
 	      // state and attempt to connect to get an updated ConnectionResult.
 	      mIntentInProgress = false;
 	      mGoogleApiClient.connect();
+	      Log.i("resolveSignInError: ", "catch");
 	    }
 	  }
 	}
 	
 	@Override
 	public void onClick(View view) {
+		Log.i("onClick: ", "started");
 		  if (view.getId() == R.id.sign_in_button
 		    && !mGoogleApiClient.isConnecting()) {
 		    mSignInClicked = true;
 		    resolveSignInError();
+		    Log.i("onClick: ", "sign in clicked");
 		  }
-		}
+		  
+		  /*if (view.getId() == R.id.sign_out_button) {
+			    if (mGoogleApiClient.isConnected()) {
+			      Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+			      mGoogleApiClient.disconnect();
+			      mGoogleApiClient.connect();
+			    }
+		  }*/
+	}
+
+	/* (non-Javadoc)
+	 * @see com.google.android.gms.common.api.ResultCallback#onResult(com.google.android.gms.common.api.Result)
+	 */
+	@Override
+	public void onResult(LoadPeopleResult arg0) {
+		// TODO Auto-generated method stub
+		Log.i("onResult: ", "started");
+	}
 }
